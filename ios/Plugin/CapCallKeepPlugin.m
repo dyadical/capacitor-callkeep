8173be30b3ea7429c7f57d69017f362b02da2b83
Luke Harold Miles <luke@cs.uky.edu>
2021-09-15 12:27:32 -0400

    add formatting to this branch

--- ios/Plugin/CapCallKeepPlugin.m
+++ ios/Plugin/CapCallKeepPlugin.m
@@ -1,8 +1,6 @@
-#import <Foundation/Foundation.h>
 #import <Capacitor/Capacitor.h>
+#import <Foundation/Foundation.h>
 
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(CapCallKeepPlugin, "CapCallKeep",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
-)
+CAP_PLUGIN(CapCallKeepPlugin, "CapCallKeep", CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);)
